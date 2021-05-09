b907f534329c1c3849956e20fa06a27942cf0f5e
FabriK12 <fbalarezo@unsa.edu.pe>
2021-05-08 21:25:48 -0500

    Se modico la funcion ver y se modifico en index.js el metodo post para interpretar mediante Markdown

--- index.js
+++ index.js
@@ -21,8 +21,29 @@ app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.post('/', (request, response) => {
+  var fileName = request.body.text;
+  console.log(fileName);
+  fs.readFile(path.resolve(__dirname, 'dat/' + fileName), 'utf8',(err, data) => {
+    if(err){
+      console.log(err);
+      response.status(500).json({
+        error: 'message'
+      })
+      return
+    }
+    let markDownText = data;
+    console.log(markDownText);
+    let htmlText = md.render(markDownText);
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({
+      text: htmlText
+    }))
+  })
+})
+
 app.get('/explorar', (request, response) => {
-  fs.readdir(path.resolve(__dirname,'pub/'), (error, files) => {
+  fs.readdir(path.resolve(__dirname,'dat/'), (error, files) => {
     if (error) {
       console.error(error)
       response.status(500).json({
@@ -35,14 +56,3 @@ app.get('/explorar', (request, response) => {
   });
 });
  
-    
-app.get('/ver', (request, response) => {
-  console.log(request.body);
-  let markDownText = request.body.text;
-  console.log(markDownText);
-  let htmlText = md.render(markDownText);
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify({
-    text: htmlText
-  }))
-});
